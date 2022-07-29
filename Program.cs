f30d85d65e8e8328c5d2aa77cf56dcfaa12a1c12
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2022-07-29 11:39:08 -0400

    Added CORS policy to Program.cs.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,16 @@ builder.Services.AddDbContext<Context>(options => options.UseDb2(connection, p =
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
+string corsPolicy = "HabitBuilder";
+
+builder.Services.AddCors(options =>
+    options.AddPolicy(
+        name: corsPolicy,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+        }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(corsPolicy);
 
 app.UseHttpsRedirection();
 
